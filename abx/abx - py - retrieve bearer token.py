7d426f51d379a3f6bc3e9e1370de174793e72262
hobovirtual <20516722+hobovirtual@users.noreply.github.com>
2021-06-09 09:21:15 -0400

    Update abx - py - retrieve bearer token.py

--- abx/abx - py - retrieve bearer token.py
+++ abx/abx - py - retrieve bearer token.py
@@ -7,21 +7,28 @@
 # 
 import requests
 import json
-
+ 
 def handler(context, inputs):
   # define headers
-  headers = {"Content-Type": "application/json"}
-    
+  headers = {"Content-Type": "application/x-www-form-urlencoded"}
+   
   # define request body
-  body = {}
-  body['refreshToken'] = context.getSecret(inputs["vra-refreshtoken"])
-    
+  payload = {'refresh_token':context.getSecret(inputs["vra-refreshtoken"])}
+ 
   # login request
-  response = requests.post(inputs['uri'], headers = headers, data = json.dumps(body), verify = False)
-    
+  response = requests.post(inputs['uri'], headers = headers, data = payload, verify = False)
+ 
   # extract bearer token
   if response.status_code == 200:
-    token = response.json().get('token')
-    outputs = {}
-    outputs['token'] = token
-    return outputs
+    token = response.json().get('access_token')
+   
+    if token:
+      outputs = {}
+      outputs['token'] = token
+      return outputs
+    else:
+      raise Exception("could not retrieve token from content response, please validate: ",token)
+     
+  else:
+    print(response.text)
+    raise Exception("error returned from rest operation: ",response.status_code)
